9775ed2f0f76dda3f611ea4b07cb3978c355de4b
ShiroBotz <96462315+AlyaaXd@users.noreply.github.com>
2022-01-06 11:30:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,10 @@
 // thank you to ALL Bot creator
 // and thanks you to who support my Bot
 
-
-global.linkGC = ['', ''] // ganti jadi group lu
+gc1 = ''
+gc2 = ''
+gc3 = ''
+global.linkGC = ['https://chat.whatsapp.com/HJ9pD2XJ8vq7whuAYOvT1Y', ''] // ganti jadi group lu
 global.owner = ['6289505165400'] // Put your number here //owner eval
 global.kontak = ['6289505165400','0'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
