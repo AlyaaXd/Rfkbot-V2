d1d8ef42301b9144759b61723e4453f28a56e335
ShiroBotz <96462315+AlyaaXd@users.noreply.github.com>
2022-01-18 21:15:59 +0700

    Update viaovo.js

--- plugins/viaovo.js
+++ plugins/viaovo.js
@@ -1,30 +1,31 @@
 let handler = async (m, { conn }) => {
-	conn.reply(m.chat, `╠═〘 PEMBAYARAN 〙 ═
-╠═ Untuk Pembayaran Via OVO
-╠➥ Ke nomer ini
-║➥ 083822759168
-║
-╠═〘 Fitur 〙 ═
-║
-╠➥ *FITUR DOWNLOADER 500++
-║     YouTube, Twitter, 
-║     Tiktok, Instagram, dll.
-║    *FITUR STIKER*
-╠➥ *FITUR ADMIN*
-║    *KICK ORANG*
-║    *JADIIN ADMIN*
-╠➥ *FITUR ISLAM*
-║   *QURAN*
-║   *CEK JADWAL SHALAT*
-╠➥ *INTERNET*
-║   *BRAINLY*
-║   *GOOGLE*
-╠➥ *DAN 200 LEBIH*
-║   *FITUR LAINNYA*
-║ 
-╠═ ©2021 Rpg wabot-aq
-╠═ Scrip original by Nurutomo
-╠═〘 ${namabot} 〙 ═`.trim(), m)
+	conn.reply(m.chat, `┏━━━━━ 「 PEMBAYARAN 」 ━━━ ❖
+┃❖ Untuk Pembayaran Via OVO
+┃❖ Ke nomer ini
+┃❖ 085730903853
+┃
+┃❖「 Fitur 」 ❖
+┃
+┃❖ *FITUR DOWNLOADER 500++
+┃     YouTube, Twitter, 
+┃     Tiktok, Instagram, dll.
+┃    *FITUR STIKER*
+┃❖ *FITUR ADMIN*
+┃   *KICK ORANG*
+┃    *JADIIN ADMIN*
+┃❖ *FITUR ISLAM*
+┃   *QURAN*
+┃   *CEK JADWAL SHALAT*
+┃❖ *INTERNET*
+┃   *BRAINLY*
+┃   *GOOGLE*
+┃❖ *DAN 200 LEBIH*
+┃   *FITUR LAINNYA*
+┃ 
+┃❖ © Rpg AlyaaXzy
+┃❖ Scrip original by Nurutomo
+┃❖「 ${namabot} 」 
+┗━━━━━━━━━━━━━━ ❖`.trim(), m)
 }
 
 handler.command = /^viaovo$/i
