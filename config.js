4718e8db61d1dc1d534a8f9521efefea041c21c3
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-01-27 00:30:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.linkGC = ['https://chat.whatsapp.com/HJ9pD2XJ8vq7whuAYOvT1Y', ''] // gant
 global.owner = ['+1 (606) 698-0053','+1 (775) 403-5736'] // Put your number here //owner eval
 global.kontak = ['+1 (606) 698-0053','0'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
-global.prems = [','+1 (775) 403-5736'6289505165400','+1 (606) 698-0053'] // Premium user has unlimited limit
+global.prems = ['+1 (775) 403-5736','6289505165400','+1 (606) 698-0053'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
