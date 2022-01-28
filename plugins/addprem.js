1df330b0a855026a15fbba75a1750f9f27f92a09
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-01-28 08:11:03 +0700

    Update addprem.js

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -1,23 +1,22 @@
-let fs = require('fs')
 let handler = async (m, { conn, text }) => {
 
-    const json = JSON.parse(fs.readFileSync('./src/premium.json'))
     let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
-    else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
-    if (json.includes(who.split`@`[0])) throw `@${who.split`@`[0]} sudah premium!`
-    json.push(`${who.split`@`[0]}`)
-    fs.writeFileSync('./src/premium.json', JSON.stringify(json))
-    m.reply(`@${who.split`@`[0]} sekarang premium!`)
-//@${m.who.split`@`[0]} ////@${who.split`@`[0]}
-    delete require.cache[require.resolve('../config')]
-    require('../config')
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
+    else who = m.chat
+    if (!who) throw `tag the person!`
+    if (global.prems.includes(who.split`@`[0])) throw 'dia sudah premium!'
+    global.prems.push(`${who.split`@`[0]}`)
+    conn.reply(m.chat, `@${who.split`@`[0]} Sekarang premium!`, m, {
+        contextInfo: {
+            mentionedJid: [who]
+        }
+    })
 
 }
 handler.help = ['addprem [@user]']
 handler.tags = ['owner']
-handler.command = /^(add|tambah|\+)prem$/i
+handler.command = /^(add|plus|\+)prem$/i
 
-handler.owner = true
+handler.rowner = true
 
 module.exports = handler
