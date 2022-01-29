0286b34bd552ac1fa49571e89c5f4cce7130d8e7
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-01-29 22:04:58 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -15,7 +15,7 @@ handler.all = async function (m, { isBlocked }) {
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
             await this.send2Button(m.chat,
-                isBanned ? `${namabot} tidak aktif` : banned ? 'kamu dibanned' : `${namabot} disini, ada yang bisa saya bantu?`,
+                isBanned ? `${namabot} tidak aktif` : banned ? 'kamu dibanned' : `AlyaaXzy di sini, Btw ngapain manggil kangen ya bg?`,
                 footer,
                 isBanned ? 'Unban' : banned ? 'Pemilik Bot' : 'Menu',
                 isBanned ? '.unban' : banned ? '.owner' : '.?',
