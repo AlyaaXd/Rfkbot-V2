685c931bc2f396a9a842b8d5eff6110bf9e7c3d4
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-01-29 23:56:37 +0700

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -1,23 +1,16 @@
 let fetch = require('node-fetch')
-     let handler  = async (m, { conn, usedPrefix, command }) => {
-m.reply(wait)
-heum = await fetch(`https://api.lolhuman.xyz/api/random/loli?apikey=${lolkey}`)
-    json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, kasihcaption, footer, 'Next', `${usedPrefix + command}`, m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 
+let handler = async (m, { conn, text }) => {
+let res = await fetch('https://raw.githubusercontent.com/Xmell91/loli/master/loli.json')
+if (!res.ok) throw await `${res.status} ${res.statusText}`;
+let json = await res.json();
+let url = json[Math.floor(Math.random() * json.length)]
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'nih', '©AlyaaXzy', 'Get Again', '/loli', m)
 }
+handler.command = /^(loli)$/i
+handler.tags = ['fun']
 handler.help = ['loli']
-handler.tags = ['anime']
-handler.command = /^loli$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
+handler.limit = true
 
 module.exports = handler
