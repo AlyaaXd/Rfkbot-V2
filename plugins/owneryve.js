57e0b7960aeae68ae4858218711b5ae3d9490625
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-01-29 21:42:52 +0700

    Delete plugins directory

--- plugins/owneryve.js
+++
@@ -1,8 +0,0 @@
-let handler = function (m) {
-	this.sendContact(m.chat, '6282297563380', 'Owner MikeBot :)', m)
-}
-
-handler.customPrefix = ['🍭Owner Mike'] 
-handler.command = new RegExp
-
-module.exports = handler
