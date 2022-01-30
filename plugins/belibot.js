1dcb686a1daab22f3ee569769b2544ee9c589b2a
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-01-31 02:08:39 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,9 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
-╠➥ *2 minggu* :      *Rp 7000*
-╠➥ *1 Bulan* :      *Rp 10000*
-╠➥ *Permanen* : *Rp 50000*
+╠➥ *1 Minggu* :     *Rp 15000*
+╠➥ *1 Bulan* :      *Rp 20000*
+╠➥ *2 Bulan* :      *Rp 30000*
+╠➥ *Permanen* : *Rp 40000*
 ╠➥ *Owner* :   *Rp 30000*/bln
 ╠➥ *Nomer Gopay & pulsa* : 
 ║    085730903853
