e94056a53f8c73305722c0864c3fbf625a76a604
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-01-30 11:35:02 +0700

    Update hentai.js

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -1,11 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
-if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
-m.reply(wait)
-heum = await fetch(`https://api.lolhuman.xyz/api/random/nsfw/waifu?apikey=${lolkey}`)
+	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
+   heum = await fetch(`https://api-alphabot.herokuapp.com/api/nsfw/hentai?apikey=Alphabot`)
    if (!heum) throw json
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, kasihcaption, footer, 'NEXT', `${usedPrefix + command}`, m)
+   conn.sendButtonImg(m.chat, json, '© AlyaaXzy', 'NEXT', `${usedPrefix + command}`, m)
 }
 handler.help = ['hentai']
 handler.tags = ['hentai']
