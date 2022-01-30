23519c5b3b1c31629d347b5fb26ff8f722c09a8e
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-01-31 01:17:05 +0700

    Update runtime.js

--- plugins/runtime.js
+++ plugins/runtime.js
@@ -1,38 +1,24 @@
-let handler  = async (m, { itsu, usedPrefix: _p }) => {
-let { performance } = require('perf_hooks')
-let fs = require('fs')
-let pelo = fs.readFileSync('./src/mike.png')
-let peli = fs.readFileSync('./src/mikey.jpg')
-function kyun(seconds){
-  function pad(s){
-    return (s < 10 ? '0' : '') + s;
-  }
-  var hours = Math.floor(seconds / (60*60));
-  var minutes = Math.floor(seconds % (60*60) / 60);
-  var seconds = Math.floor(seconds % 60);
-
-  //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
-  return `${pad(hours)}Jam ${pad(minutes)}Menit ${pad(seconds)}Detik`
-}
-					runtime = process.uptime()
-					teks = `${global.packname}\n\n${kyun(runtime)}`
-					var itsme = `0@s.whatsapp.net`
-					var split = `teks`
-					const rtimebro = {
-					contextInfo: {
-					participant: itsme,
-					quotedMessage: {
-					extendedTextMessage: {
-					text: split,
-									}
-								}
-							}
-					}
-					m.reply(teks)
-}
-
-handler.help = ['runtime']
-handler.tags = ['info']
-handler.command = /^runtime$/i
+let handler = async (m, { usedPrefix, command }) => {
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
+let runnya = `
+*───「 RUNTIME BOT 」───*
+
+Time : ${time}
+Runtime : ${uptime}
+`
+conn.sendButton(m.chat, runnya, watermark, 'Menu', '.menu', m) 
+}
+handler.help = ['runtime']
+handler.tags = ['info']
+handler.command = /^(uptime|runtime)$/i
 
 module.exports = handler
+
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
