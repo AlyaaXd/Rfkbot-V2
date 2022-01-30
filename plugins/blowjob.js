331406a5bacdbb166d301267dcee492ee63a149e
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-01-30 11:38:12 +0700

    Update blowjob.js

--- plugins/blowjob.js
+++ plugins/blowjob.js
@@ -1,9 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
-   heum = await fetch(`https://api.lolhuman.xyz/api/random2/bj?apikey=Dawnfrostkey`)
+   heum = await fetch(`https://api.lolhuman.xyz/api/random2/bj?apikey=rey2k21`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '🔥🥵🥵', '©mikebot', 'NEXT', `${usedPrefix + command}`, m)
+   conn.sendButtonImg(m.chat, json, 'Ahh jadi sange kak', watermark, 'NEXT', `${usedPrefix + command}`, m)
 }
 handler.help = ['blowjob']
 handler.tags = ['hentai']
@@ -14,7 +14,6 @@ handler.admin = false
 handler.botAdmin = false
 
 handler.fail = null
-handler.limit = true
+handler.limit = 30
 
 module.exports = handler
-
