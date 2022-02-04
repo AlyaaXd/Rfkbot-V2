30e39299f6efdd47f2912a5e938ac710ce9e70d2
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-04 17:07:04 +0700

    Update and rename gb.js to vah.js

--- plugins/vah.js
+++ plugins/vah.js
@@ -1,6 +1,6 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 ye = `@${m.sender.split`@`[0]}`
-let info = `Hai Kak ${ye} Nih Sc ini di jual 30k minat? chat Owner >///<
+let info = `Hai Kak ${ye} Nih Sc ini menggunakan SC dari https://github.com/AlyaaXd/Rfkbot-V2 Jan lupa follow yah
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', `*${global.packname}*`, 'status@broadcast') 
